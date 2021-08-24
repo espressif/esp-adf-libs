3e7593b0776a9a0df603e4fe31611f6c09aa4fd0
majingjing <majingjing@espressif.com>
2021-08-24 20:46:37 +0800

    ID:[a07d753e] 1 fix a bug can't seek due to instream size too samller 2 fix a bug that running time is incorrect 3 fix a bug that alternate use the in stream on perfer memory type 4 fix a bug if insert play in speed mode, last play's decoder output data is written to ringbuf 5 fix a bug that m4a_get_pos appear crash 6 fix a bug that in encode case, set stream info by setup info incorrect 7 add some encode config interface for user

--- esp_codec/include/codec/g711_encoder.h
+++ esp_codec/include/codec/g711_encoder.h
@@ -12,6 +12,16 @@ extern "C" {
 #endif
 
 /**
+ * @brief      Enum of G711 Encoder enc mode 
+ */
+typedef enum {
+    G711_ENC_MODE_UNKNOW  = -1,   /*!< Invalid mode */
+    G711_ENC_MODE_A_LOW   = 0,    /* a-law */
+    G711_ENC_MODE_U_LOW   = 1,    /* u-law */
+    G711_ENC_MODE_N_MODE  = 2,    /* Not Used  */
+} g711_encoder_mode_t;
+
+/**
  * @brief      G711 Encoder configurations
  */
 typedef struct {
@@ -28,14 +38,27 @@ typedef struct {
 #define G711_ENCODER_TASK_PRIO           (5)
 #define G711_ENCODER_RINGBUFFER_SIZE     (2 * 1024)
 
-#define DEFAULT_G711_ENCODER_CONFIG() {\
-    .out_rb_size        = G711_ENCODER_RINGBUFFER_SIZE,\
-    .task_stack         = G711_ENCODER_TASK_STACK,\
-    .task_core          = G711_ENCODER_TASK_CORE,\
-    .task_prio          = G711_ENCODER_TASK_PRIO,\
-    .stack_in_ext       = true,\
-    .enc_mode           = 0,\
+#define DEFAULT_G711_ENCODER_CONFIG() {                 \
+    .out_rb_size        = G711_ENCODER_RINGBUFFER_SIZE, \
+    .task_stack         = G711_ENCODER_TASK_STACK,      \
+    .task_core          = G711_ENCODER_TASK_CORE,       \
+    .task_prio          = G711_ENCODER_TASK_PRIO,       \
+    .stack_in_ext       = true,                         \
+    .enc_mode           = G711_ENC_MODE_A_LOW,          \
 }
+
+/**
+ * @brief      Set G711 encoder mode
+ *
+ * @param      self        Audio element handle
+ *
+ * @param      enc_mode    Encode mode choose, value from g711_encoder_mode_t
+ *
+ * @return     
+ *             ESP_OK
+ *             ESP_FAIL
+ */
+esp_err_t g711_encoder_set_enc_mode(audio_element_handle_t self, g711_encoder_mode_t enc_mode);
 
 /**
  * @brief      Create a handle to an Audio Element to encode incoming data using G711 format
