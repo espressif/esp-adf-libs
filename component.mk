32983768e9f3c87bdcdd6bdd1d46d51ff46451a3
maojianxin <maojianxin@espressif.com>
2024-01-23 21:55:05 +0800

    ID:427bb54c Remove recorder engine codes that is not being used

--- component.mk
+++ component.mk
@@ -5,7 +5,6 @@
 COMPONENT_ADD_INCLUDEDIRS :=    esp_audio/include \
                                 esp_codec/include/codec \
                                 esp_codec/include/processing \
-                                recorder_engine/include \
                                 media_lib_sal/include \
                                 media_lib_sal/include/port \
                                 audio_misc/include \
@@ -24,21 +23,9 @@ ifdef CONFIG_MEDIA_PROTOCOL_LIB_ENABLE
     COMPONENT_SRCDIRS += esp_media_protocols
 endif
 
-ifdef CONFIG_REC_ENG_ENABLE_VAD_ONLY
- LIBS += recorder_engine_vad
-endif
-
-ifdef CONFIG_REC_ENG_ENABLE_VAD_WWE
- LIBS += recorder_engine_vad_wwe
-endif
-
-ifdef CONFIG_REC_ENG_ENABLE_VAD_WWE_AMR
- LIBS += recorder_engine
-endif
 
 COMPONENT_ADD_LDFLAGS +=  -L$(COMPONENT_PATH)/esp_audio/lib/esp32 \
                           -L$(COMPONENT_PATH)/esp_codec/lib/esp32 \
-                          -L$(COMPONENT_PATH)/recorder_engine/lib/esp32 \
                           -L$(COMPONENT_PATH)/esp_media_protocols/lib/esp32 \
                           -L$(COMPONENT_PATH)/esp_muxer/lib/esp32 \
                            $(addprefix -l,$(LIBS)) \
